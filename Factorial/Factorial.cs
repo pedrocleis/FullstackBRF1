96734834218a058f6354ab28fdfcf9fb77d91c75
Pedro Cleis <pedrokleiz@gmail.com>
2020-07-07 19:37:27 -0300

    Addind the code

---
+++ Factorial/Factorial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace Factorial
+{
+    public class Factorial
+    {
+        public BigInteger factorial(BigInteger number)
+        {
+            // Console.WriteLine(number);
+            if (number < 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * factorial(number - 1);
+
+            }
+        }
+    }
+}
